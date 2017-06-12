20e8256003a6d1c732c89a28af3065dcaaa68be3
Julian Calaby <julian.calaby@gmail.com>
2017-06-12 22:20:14 +1000

    Add create method to create new articles

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -9,11 +9,13 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Routing\ClassResourceInterface;
 use FOS\RestBundle\Util\Codes;
 use FOS\RestBundle\View\View;
+use FOS\RestBundle\Request\ParamFetcher;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type;
 use AppBundle\Entity\Article;
 
 class ArticleController extends Controller
@@ -47,5 +49,53 @@ class ArticleController extends Controller
         }
 
         return $article;
+    }
+
+    /**
+     * Post new article action
+     * 
+     * @Rest\View()
+     * @Rest\RequestParam(name="author_id", nullable=false)
+     * @Rest\RequestParam(name="title", nullable=false)
+     * @Rest\RequestParam(name="content", nullable=false)
+     *
+     * @param ParamFetcher $fetcher
+     */
+    public function postArticleAction(ParamFetcher $fetcher)
+    {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article, array('csrf_protection' => false))
+            ->add("author", 'Symfony\Bridge\Doctrine\Form\Type\EntityType', array(
+                'class' => 'AppBundle:Author'
+            ))
+            ->add("title", Type\TextType::class)
+            ->add("content", Type\TextType::class)
+            ->getForm();
+
+        $data = $fetcher->all();
+
+        if (isset($data["author_id"])) {
+            $data["author"] = $data["author_id"];
+            unset($data["author_id"]);
+        }
+
+        $form->submit($data, true);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $article->setCreatedAt(new \DateTime());
+            $article->setUpdatedAt(new \DateTime());
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('get_article', array("id" => $article->getId()));
+        }
+
+        return $form;
     }
 }
--- tests/AppBundle/Controller/ArticleControllerTest.php
+++ tests/AppBundle/Controller/ArticleControllerTest.php
@@ -99,4 +99,93 @@ class ArticleControllerTest extends WebTestCase
 
         $this->assertEquals($expected, $actual);
     }
+
+    /**
+     * Given the articles REST endpoint
+     * When given data to create a new article
+     * Then the data returned will match the data given
+     */
+    public function testCreateArticle()
+    {
+        $this->loadFixtures(array(
+            'AppBundle\DataFixtures\ORM\TwoAuthorsData'
+        ));
+
+        $client = static::createClient();
+
+        $client->followRedirects();
+
+        $client->request(
+            'POST',
+            '/articles',
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            '{"author_id":1,"title":"test post article","content":"Some test article"}'
+        );
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $actual = json_decode($response->getContent(), true);
+
+        $this->assertEquals("Frank Matthews", $actual["author"]);
+        $this->assertEquals("test post article", $actual["title"]);
+        $this->assertEquals("Some test article", $actual["content"]);
+    }
+
+    /**
+     * Given the articles REST endpoint
+     * When given data with an unknown author to create a new article
+     * Then the request will fail
+     */
+    public function testCreateArticleUnknownAuthor()
+    {
+        $this->loadFixtures(array());
+
+        $client = static::createClient();
+
+        $client->followRedirects();
+
+        $client->request(
+            'POST',
+            '/articles',
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            '{"author_id":9999,"title":"test post article","content":"Some test article"}'
+        );
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(400, $response->getStatusCode());
+    }
+
+    /**
+     * Given the articles REST endpoint
+     * When given invalid data to create a new article
+     * Then the request will fail
+     */
+    public function testCreateArticleFailure()
+    {
+        $this->loadFixtures(array());
+
+        $client = static::createClient();
+
+        $client->followRedirects();
+
+        $client->request(
+            'POST',
+            '/articles',
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            '{"invalid": "INVALID"}'
+        );
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(400, $response->getStatusCode());
+    }
 }
